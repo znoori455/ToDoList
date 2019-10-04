6315b1acb7d5f0d965158d3ed82da2eee7c6d56c
znoori455 <znoori455@gmail.com>
2019-10-03 21:49:47 -0400

    basic structure and functionality completed

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,1 +1,22 @@
-alert('connected');
+//check off specific todos by clicking
+$("ul").on("click", "li", function() {
+    $(this).toggleClass("completed");
+});
+
+//click on X to delete todo
+$("ul").on("click", "span",function(event) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+    if( event.which === 13 ) {
+        //grabbing new todo text from input
+        let todoText = $(this).val();
+        $(this).val("");
+        //create a new li and add to ul
+        $("ul").append(`<li><span>X</span> ${todoText}</li>`);
+    }
+});
